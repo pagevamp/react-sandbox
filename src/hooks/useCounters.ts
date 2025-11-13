1fe996b249f91d8a04ff9b2a67c34f9f5f5dbd0b
Laxman Rumba <rumbavai767@gmail.com>
2025-11-13 21:51:48 +0545

    refact/updated counter to use localstorage to persist the count on reload

--- src/hooks/useCounters.ts
+++ src/hooks/useCounters.ts
@@ -1,7 +1,15 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useCounter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => {
+    const storedCount = localStorage.getItem("count");
+    return storedCount ? parseInt(storedCount, 10) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("count", count.toString());
+  }, [count]);
+
   const handleIncrement = useCallback(() => setCount((count) => count + 1), []);
   const handleDecrement = useCallback(
     () => setCount((count) => Math.max(count - 1, 0)),
