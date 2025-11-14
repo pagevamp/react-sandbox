b99ea41e7288d9e159d591ac5679e16dea9902b9
Laxman Rumba <rumbavai767@gmail.com>
2025-11-14 07:12:26 +0545

    refact/updated the useItems hook and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,31 @@
 import { useEffect, useRef, useState } from "react";
 import Counter from "./components/Counter/Counter";
+import { AddItem } from './components/Item/AddItem/AddItem';
+import { useItems } from "./hooks/useItems";
+import Button from "./components/Button/Button";
+import { ItemList } from "./components/Item/ItemList/ItemList";
 
 export default function App() {
-  const [count, setCount] = useState(0);
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [isVisible, setIsVisible] = useState(true);
-  const [items, setItems] = useState<string[]>([]);
-  const [selectedItem, setSelectedItem] = useState<string | null>(null);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    const storedCount = localStorage.getItem("count");
-    if (storedCount) {
-      setCount(parseInt(storedCount));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("count", count.toString());
-  }, [count]);
-
-  useEffect(() => {
-    document.title = `Hello ${name}`;
-  }, [name]);
-
-  useEffect(() => {
-    if (selectedItem) {
-      console.log(selectedItem);
-    }
-  }, [selectedItem]);
-
   useEffect(() => {
     document.body.classList.toggle("dark", isDarkMode);
   }, [isDarkMode]);
 
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
-
-  function addItem() {
-    if (inputRef.current && inputRef.current.value) {
-      setItems([...items, inputRef.current.value]);
-      inputRef.current.value = "";
-    }
-  }
-
-  async function handleSubmit() {
-    // Do something with data...
-  }
-
+  const { items, isVisible, inputValue, setInputValue,setSelectedItem, addItem, toggleVisibility } = useItems();
   return (
     <div>
-      <Counter/>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            value={name}
-            onChange={handleNameChange}
-            placeholder="Enter your name"
-          />
-          <p>Name: {name}</p>
-        </div>
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            placeholder="Enter your email"
-          />
-          <p>Email: {email}</p>
-        </div>
-        <div>
-          <div>
-            <button onClick={toggleVisibility}>
-              {isVisible ? "Hide" : "Show"} Items
-            </button>
-            {isVisible && (
-              <div>
-                <input type="text" ref={inputRef} placeholder="Add item" />
-                <button onClick={addItem}>Add Item</button>
-                <ul>
-                  {items.map((item, index) => (
-                    <li key={index} onClick={() => setSelectedItem(item)}>
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </div>
-          <div>
-            <p>Selected Item: {selectedItem}</p>
-          </div>
-        </div>
-      </form>
+      <Counter />
+      <div className="items">
+        <Button className="toggle-visible-btn" value={isVisible ? "Hide" : "Show"} onClick={toggleVisibility}></Button>
+        {isVisible && (
+          <>
+            <AddItem addItem={addItem} inputValue={inputValue} setInputValue={setInputValue} />
+            <ItemList items={items} setSelectedItem={setSelectedItem}/>
+          </>
+        )}
+      </div>
+
       <div>
         <button onClick={() => setIsDarkMode(!isDarkMode)}>
           Toggle Dark Mode
--- src/components/Item/ItemList/ItemList.tsx
+++ src/components/Item/ItemList/ItemList.tsx
@@ -1,9 +1,15 @@
-const ItemList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import styles from './style.module.css'
+interface ItemListProps {
+  items: string[];
+  setSelectedItem: (item: string) => void;
 }
 
-export default ItemList
+export const ItemList = ({ items, setSelectedItem }: ItemListProps) => (
+  <ul className={styles["item-list"]}>
+    {items.map((item, index) => (
+      <li key={index} className={styles["item-list__item"]} onClick={() => setSelectedItem(item)}>
+        {item}
+      </li>
+    ))}
+  </ul>
+);
