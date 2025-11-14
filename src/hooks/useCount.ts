2df6f59797b249044b1f82ab91e99b2b4bd50902
thisme8 <stutiupreti8@gmail.com>
2025-11-14 12:33:56 +0545

    Feat(Count): Add count feature

--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -1,1 +1,26 @@
-export default function useCount() {}
+import { useCallback, useEffect, useState } from "react"
+
+export default function useCount() {
+  const [count, setCount] = useState(0)
+
+  const onIncrement = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+
+  const onDecrement = useCallback(() => {
+    setCount(count - 1)
+  }, [count])
+
+  useEffect(() => {
+    localStorage.setItem("count", count.toString())
+  }, [count])
+
+  useEffect(() => {
+    const storedCount = localStorage.getItem("count")
+    if (storedCount) {
+      setCount(parseInt(storedCount))
+    }
+  }, [])
+
+  return { count, onIncrement, onDecrement }
+}
