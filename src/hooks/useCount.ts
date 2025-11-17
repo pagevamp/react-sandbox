60b67637a43d395f0762e23edc051129635f5287
thisme8 <stutiupreti8@gmail.com>
2025-11-17 11:42:23 +0545

    Remove redundant code

--- src/hooks/useCount.ts
+++ src/hooks/useCount.ts
@@ -1,26 +1,25 @@
 import { useCallback, useEffect, useState } from "react"
 
 export default function useCount() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(() => {
+    const storedCount = localStorage.getItem("count")
+    if (storedCount) {
+      return parseInt(storedCount)
+    }
+    return 0
+  })
 
   const onIncrement = useCallback(() => {
-    setCount(count + 1)
+    setCount(count! + 1)
   }, [count])
 
   const onDecrement = useCallback(() => {
-    setCount((count) => Math.max(count - 1, 0))
+    setCount((count) => Math.max(count! - 1, 0))
   }, [count])
 
   useEffect(() => {
-    localStorage.setItem("count", count.toString())
+    localStorage.setItem("count", count!.toString())
   }, [count])
 
-  useEffect(() => {
-    const storedCount = localStorage.getItem("count")
-    if (storedCount) {
-      setCount(parseInt(storedCount))
-    }
-  }, [])
-  
   return { count, onIncrement, onDecrement }
 }
